a34424e44c7a965bf93a309069c2ed9fd64837e6
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-05 17:29:13 +0530

    Dec5 - Virtual Threads sample code

---
+++ JVMperfomance/src/performance/Main.java
@@ -0,0 +1,9 @@
+package performance;
+
+public class Main {
+	public static void main(String[] args) {
+		Worker w1 = new Worker();
+		Thread t1 = new Thread();
+		t1.start();
+	}
+}
---
+++ JVMperfomance/src/performance/Worker.java
@@ -0,0 +1,15 @@
+package performance;
+
+public class Worker implements Runnable{
+	int add(int a, int b) {
+		add(1, 2);
+		return a + b;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
