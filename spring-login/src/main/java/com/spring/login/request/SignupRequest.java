f4037814b9ea969091d6b727e633e7a09f86973a
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-06 10:14:45 +0530

    Dec6 - Signup Dto added

---
+++ spring-login/src/main/java/com/spring/login/request/SignupRequest.java
@@ -0,0 +1,27 @@
+package com.spring.login.request;
+
+import java.util.Set;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+	@NotBlank
+	@Size(min = 1, max = 20)
+	private String username;
+	@NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+	private Set<String> role;
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
