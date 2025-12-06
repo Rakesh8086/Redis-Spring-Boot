b752add0991a7f4f1fb3db5c959185c80301c8af
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-06 10:45:31 +0530

    Dec6 - Added constructor in User and a message response dto

--- spring-login/src/main/java/com/spring/login/model/User.java
+++ spring-login/src/main/java/com/spring/login/model/User.java
@@ -47,4 +47,11 @@ public class User {
 	             joinColumns = @JoinColumn(name = "user_id"),
 	             inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+	
+	// this constructor is needed when user does not specify role
+	public User(String username, String email, String password) {
+	    this.username = username;
+	    this.email = email;
+	    this.password = password;
+	}
 }
