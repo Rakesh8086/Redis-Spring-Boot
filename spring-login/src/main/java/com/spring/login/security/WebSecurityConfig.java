1915f431c714086182ec66600201888b6479e179
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-06 15:07:16 +0530

    Dec6 - Jwt authenticator during protected endpoint access and jwt error responder added

--- spring-login/src/main/java/com/spring/login/security/WebSecurityConfig.java
+++ spring-login/src/main/java/com/spring/login/security/WebSecurityConfig.java
@@ -1,18 +1,31 @@
 package com.spring.login.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.spring.login.security.jwt.AuthTokenFilter;
+import com.spring.login.security.jwt.JwtAuthErrorResponder;
+import com.spring.login.security.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
+	@Autowired
+	private JwtAuthErrorResponder unauthorizedHandler;
+	
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
 	   return authConfig.getAuthenticationManager();
@@ -24,14 +37,35 @@ public class WebSecurityConfig {
 	}
 	
 	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+	      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+	       
+	      authProvider.setUserDetailsService(userDetailsService);
+	      authProvider.setPasswordEncoder(passwordEncoder());
+	   
+	      return authProvider;
+	}
+	
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+	    return new AuthTokenFilter();
+	}
+	
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	    http
 	      .csrf(csrf -> csrf.disable())
+	      .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+	      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 	      .authorizeHttpRequests(auth ->
 	        auth.requestMatchers("/api/auth/**").permitAll()
+	        	.requestMatchers("/api/test/**").permitAll()
 	            .anyRequest().authenticated()
 	      );
-	
+	    
+	    http.authenticationProvider(authenticationProvider());
+	    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	    
 	    return http.build();
 	}
 }
