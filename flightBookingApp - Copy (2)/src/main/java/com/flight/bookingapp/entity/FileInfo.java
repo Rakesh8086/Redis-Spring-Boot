b3a550f842f8c57712c7871fe9381bf04e97494b
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-11 15:54:12 +0530

    Dec11 - file upload and get files from storage implemented

---
+++ flightBookingApp - Copy (2)/src/main/java/com/flight/bookingapp/entity/FileInfo.java
@@ -0,0 +1,13 @@
+package com.flight.bookingapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileInfo {
+	private String name;
+	private String url;
+}
